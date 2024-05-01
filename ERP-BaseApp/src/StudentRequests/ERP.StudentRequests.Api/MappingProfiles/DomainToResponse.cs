0f6bb413f5ca0ee70a12974cdb98c658d97c4020
Sandali <sandali811@gmail.com>
2024-05-01 17:59:03 +0530

    added request forum mapping profiles

---
+++ ERP-BaseApp/src/StudentRequests/ERP.StudentRequests.Api/MappingProfiles/DomainToResponse.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ERP.StudentRequests.Core.DTOs.Response;
+using ERP.StudentRequests.Core.Entity;
+
+namespace ERP.StudentRequests.Api.MappingProfiles
+{
+    public class DomainToResponse : Profile
+    {
+        public DomainToResponse()
+        {
+            CreateMap<Request, GetReqLetterResponse>();
+
+            CreateMap<Student, GetStudentResponse>()
+                .ForMember(
+                dest => dest.StudentId,
+                opt => opt.MapFrom(src => src.Id));
+
+            CreateMap<Lecturer, GetLecturerResponse>()
+                .ForMember(
+                dest => dest.LecturerId,
+                opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
---
+++ ERP-BaseApp/src/StudentRequests/ERP.StudentRequests.Api/MappingProfiles/RequestToDomain.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using ERP.StudentRequests.Core.DTOs.Request;
+using ERP.StudentRequests.Core.Entity;
+
+namespace ERP.StudentRequests.Api.MappingProfiles
+{
+    public class RequestToDomain : Profile
+    {
+        public RequestToDomain()
+        {
+            CreateMap<CreateReqLetterRequest, Request>()
+                .ForMember(
+                dest => dest.Status,
+                opt => opt.MapFrom(src => 1))
+                .ForMember(
+                dest => dest.AddedDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(
+                dest => dest.UpdatedDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+
+            ;
+
+            CreateMap<UpdateReqLetterRequest, Request>()
+                .ForMember(
+                dest => dest.UpdatedDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+            ;
+
+            CreateMap<CreateStudentRequest, Student>()
+                 .ForMember(
+                dest => dest.Status,
+                opt => opt.MapFrom(src => 1))
+                .ForMember(
+                dest => dest.AddedDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(
+                dest => dest.UpdatedDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+            ;
+
+            CreateMap<UpdateStudentRequest, Student>()
+                .ForMember(
+                dest => dest.UpdatedDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+            ;
+
+            CreateMap<CreateLecturerRequest, Lecturer>()
+                 .ForMember(
+                dest => dest.Status,
+                opt => opt.MapFrom(src => 1))
+                .ForMember(
+                dest => dest.AddedDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(
+                dest => dest.UpdatedDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+            ;
+
+            CreateMap<UpdateLecturerRequest, Lecturer>()
+                .ForMember(
+                dest => dest.UpdatedDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+            ;
+        }
+    }
+}
