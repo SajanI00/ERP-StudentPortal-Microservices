19d2f922e79a5d9292345a0d0da372c98984ae77
Sandali <sandali811@gmail.com>
2024-05-01 22:44:33 +0530

    student Crud in request forum

---
+++ ERP-BaseApp/src/StudentRequests/ERP.StudentRequests.Api/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using ERP.StudentRequests.Core.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.StudentRequests.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+        protected readonly IMapper _mapper;
+
+        public BaseController(
+            IUnitOfWork unitOfWork,
+            IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+    }
+}
