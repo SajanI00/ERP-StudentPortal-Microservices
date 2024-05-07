a10ed0c8e0bff62e57a0619bd00c426a61b12acb
Sandali <sandali811@gmail.com>
2024-05-07 13:28:29 +0530

    created announcement controllers

--- ERP-BaseApp/src/Announcements/ERP.Announcements.APi/Controllers/AnnouncementGroupController.cs
+++ ERP-BaseApp/src/Announcements/ERP.Announcements.APi/Controllers/AnnouncementGroupController.cs
@@ -1,90 +1,90 @@
-﻿//using AutoMapper;
-//using ERP.Announcements.Core.DTOs.Request;
-//using ERP.Announcements.Core.DTOs.Response;
-//using ERP.Announcements.Core.Entity;
-//using ERP.Announcements.Core.Interfaces;
-//using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using ERP.Announcements.Core.DTOs.Request;
+using ERP.Announcements.Core.DTOs.Response;
+using ERP.Announcements.Core.Entity;
+using ERP.Announcements.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace ERP.Announcements.Api.Controllers
-//{
-//    public class AnnouncementGroupController : BaseController
-//    {
-//        public AnnouncementGroupController(
-//            IUnitOfWork unitOfWork, 
-//            IMapper mapper) : base(unitOfWork, mapper)
-//        {
-//        }
+namespace ERP.Announcements.Api.Controllers
+{
+    public class AnnouncementGroupController : BaseController
+    {
+        public AnnouncementGroupController(
+            IUnitOfWork unitOfWork,
+            IMapper mapper) : base(unitOfWork, mapper)
+        {
+        }
 
-//        [HttpGet]
-//        [Route("{announcementGroupId:guid}")]
-//        public async Task<IActionResult> GetAnnouncementGroupMethod(Guid announcementGroupId)
-//        {
-//            var announcementGroup = await _unitOfWork.AnnouncementGroups.GetById(announcementGroupId);
+        [HttpGet]
+        [Route("{announcementGroupId:guid}")]
+        public async Task<IActionResult> GetAnnouncementGroupMethod(Guid announcementGroupId)
+        {
+            var announcementGroup = await _unitOfWork.AnnouncementGroups.GetById(announcementGroupId);
 
-//            if (announcementGroup == null)
-//            {
-//                return NotFound();
-//            }
+            if (announcementGroup == null)
+            {
+                return NotFound();
+            }
 
-//            var result = _mapper.Map<GetAnnouncementGroupResponse>(announcementGroup);
+            var result = _mapper.Map<GetAnnouncementGroupResponse>(announcementGroup);
 
-//            return Ok(result);
-//        }
+            return Ok(result);
+        }
 
-//        [HttpPost("")]
-//        public async Task<IActionResult> AddAnnouncementGroupMethod([FromBody] CreateAnnouncementGroupRequest announcementGroup)
-//        {
-//            if (!ModelState.IsValid)
-//                return BadRequest();
+        [HttpPost("")]
+        public async Task<IActionResult> AddAnnouncementGroupMethod([FromBody] CreateAnnouncementGroupRequest announcementGroup)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
 
-//            var result = _mapper.Map<AnnouncementGroup>(announcementGroup);
+            var result = _mapper.Map<AnnouncementGroup>(announcementGroup);
 
-//            await _unitOfWork.AnnouncementGroups.Add(result);
-//            await _unitOfWork.CompleteAsync();
+            await _unitOfWork.AnnouncementGroups.Add(result);
+            await _unitOfWork.CompleteAsync();
 
-//            return CreatedAtAction(nameof(GetAnnouncementGroupMethod), new { announcementGroupId = result.Id }, result);
+            return CreatedAtAction(nameof(GetAnnouncementGroupMethod), new { announcementGroupId = result.Id }, result);
 
-//        }
+        }
 
-//        [HttpPut("")]
-//        public async Task<IActionResult> UpdateAnnouncementGroupMethod([FromBody] UpdateAnnouncementGroupRequest announcementGroup)
-//        {
-//            if (!ModelState.IsValid)
-//                return BadRequest();
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateAnnouncementGroupMethod([FromBody] UpdateAnnouncementGroupRequest announcementGroup)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
 
-//            var result = _mapper.Map<AnnouncementGroup>(announcementGroup);
+            var result = _mapper.Map<AnnouncementGroup>(announcementGroup);
 
-//            await _unitOfWork.AnnouncementGroups.Update(result);
-//            await _unitOfWork.CompleteAsync();
+            await _unitOfWork.AnnouncementGroups.Update(result);
+            await _unitOfWork.CompleteAsync();
 
-//            return NoContent();
+            return NoContent();
 
-//        }
+        }
 
-//        [HttpGet]
-//        public async Task<IActionResult> GetAllAnnouncementGroupsMethod()
-//        {
-//            var announcementGroup = await _unitOfWork.AnnouncementGroups.GetAll();
+        [HttpGet]
+        public async Task<IActionResult> GetAllAnnouncementGroupsMethod()
+        {
+            var announcementGroup = await _unitOfWork.AnnouncementGroups.GetAll();
 
-//            return Ok(_mapper.Map<IEnumerable<GetAnnouncementGroupResponse>>(announcementGroup));
-//        }
+            return Ok(_mapper.Map<IEnumerable<GetAnnouncementGroupResponse>>(announcementGroup));
+        }
 
-//        [HttpDelete]
-//        [Route("{announcementGroupId:guid}")]
-//        public async Task<IActionResult> DeleteAnnouncementGroupMethod(Guid announcementGroupId)
-//        {
-//            var announcementGroup = await _unitOfWork.AnnouncementGroups.GetById(announcementGroupId);
+        [HttpDelete]
+        [Route("{announcementGroupId:guid}")]
+        public async Task<IActionResult> DeleteAnnouncementGroupMethod(Guid announcementGroupId)
+        {
+            var announcementGroup = await _unitOfWork.AnnouncementGroups.GetById(announcementGroupId);
 
-//            if (announcementGroup == null)
-//            {
-//                return NotFound();
-//            }
+            if (announcementGroup == null)
+            {
+                return NotFound();
+            }
 
-//            await _unitOfWork.AnnouncementGroups.Delete(announcementGroupId);
-//            await _unitOfWork.CompleteAsync();
+            await _unitOfWork.AnnouncementGroups.Delete(announcementGroupId);
+            await _unitOfWork.CompleteAsync();
 
-//            return NoContent();
-//        }
+            return NoContent();
+        }
 
-//    }
-//}
+    }
+}
