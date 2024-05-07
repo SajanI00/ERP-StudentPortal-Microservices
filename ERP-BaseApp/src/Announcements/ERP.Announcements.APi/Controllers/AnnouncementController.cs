8f6714274b39edbb4d6817d757dd3c4f33ce4f53
Sandali <sandali811@gmail.com>
2024-05-07 13:29:42 +0530

    completed announcements backend

--- ERP-BaseApp/src/Announcements/ERP.Announcements.APi/Controllers/AnnouncementController.cs
+++ ERP-BaseApp/src/Announcements/ERP.Announcements.APi/Controllers/AnnouncementController.cs
@@ -1,92 +1,92 @@
-﻿//using AutoMapper;
-//using ERP.Announcements.Core.DTOs.Request;
-//using ERP.Announcements.Core.DTOs.Response;
-//using ERP.Announcements.Core.Entity;
-//using ERP.Announcements.Core.Interfaces;
-//using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using ERP.Announcements.Core.DTOs.Request;
+using ERP.Announcements.Core.DTOs.Response;
+using ERP.Announcements.Core.Entity;
+using ERP.Announcements.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace ERP.Announcements.Api.Controllers
-//{
-//    public class AnnouncementController : BaseController
-//    {
-//        public AnnouncementController(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
-//        {
-//        }
+namespace ERP.Announcements.Api.Controllers
+{
+    public class AnnouncementController : BaseController
+    {
+        public AnnouncementController(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+        {
+        }
 
-//        [HttpGet]
-//        [Route("Students/{studentId:guid}")]
-//        public async Task<IActionResult> GetStudentAnnouncements(Guid studentId)
-//        {
-        
-//                var announcements = await _unitOfWork.Announcements.GetStudentAnnouncementAsync(studentId);
+        [HttpGet]
+        [Route("Students/{studentId:guid}")]
+        public async Task<IActionResult> GetStudentAnnouncements(Guid studentId)
+        {
 
-//                if (announcements == null || !announcements.Any())
-//                    return NotFound("Announcements not found");
+            var announcements = await _unitOfWork.Announcements.GetStudentAnnouncementAsync(studentId);
 
-//                var result = _mapper.Map<IEnumerable<GetAnnouncementResponse>>(announcements);
+            if (announcements == null || !announcements.Any())
+                return NotFound("Announcements not found");
 
-//                return Ok(result);
-         
+            var result = _mapper.Map<IEnumerable<GetAnnouncementResponse>>(announcements);
 
-//        }
+            return Ok(result);
 
-//        [HttpGet]
-//        [Route("Senders/{senderId:guid}")]
-//        public async Task<IActionResult> GetSenderAnnouncements(Guid senderId)
-//        {
-//            var announcements = await _unitOfWork.Announcements.GetSenderAnnouncementAsync(senderId);
 
-//            if (announcements == null || !announcements.Any())
-//                return NotFound("Announcements not found");
+        }
 
-//            var result = _mapper.Map<IEnumerable<GetAnnouncementResponse>>(announcements);
+        [HttpGet]
+        [Route("Senders/{senderId:guid}")]
+        public async Task<IActionResult> GetSenderAnnouncements(Guid senderId)
+        {
+            var announcements = await _unitOfWork.Announcements.GetSenderAnnouncementAsync(senderId);
 
-//            return Ok(result);
+            if (announcements == null || !announcements.Any())
+                return NotFound("Announcements not found");
 
-//        }
+            var result = _mapper.Map<IEnumerable<GetAnnouncementResponse>>(announcements);
 
-//        [HttpGet]
-//        [Route("AnnouncementGroups/{announcementGroupId:guid}")]
-//        public async Task<IActionResult> GetAnnouncementGroupAnnouncements(Guid announcementGroupId)
-//        {
-//            var announcements = await _unitOfWork.Announcements.GetAnnouncementGroupAnnouncementAsync(announcementGroupId);
+            return Ok(result);
 
-//            if (announcements == null || !announcements.Any())
-//                return NotFound("Announcements not found");
+        }
 
-//            var result = _mapper.Map<IEnumerable<GetAnnouncementResponse>>(announcements);
+        [HttpGet]
+        [Route("AnnouncementGroups/{announcementGroupId:guid}")]
+        public async Task<IActionResult> GetAnnouncementGroupAnnouncements(Guid announcementGroupId)
+        {
+            var announcements = await _unitOfWork.Announcements.GetAnnouncementGroupAnnouncementAsync(announcementGroupId);
 
-//            return Ok(result);
+            if (announcements == null || !announcements.Any())
+                return NotFound("Announcements not found");
 
-//        }
+            var result = _mapper.Map<IEnumerable<GetAnnouncementResponse>>(announcements);
 
-//        [HttpPost("")]
-//        public async Task<IActionResult> AddAnnouncement([FromBody] CreateAnnouncementRequest announcement)
-//        {
-//            if (!ModelState.IsValid)
-//                return BadRequest();
+            return Ok(result);
 
-//            var result = _mapper.Map<Announcement>(announcement);
+        }
 
-//            await _unitOfWork.Announcements.Add(result);
-//            await _unitOfWork.CompleteAsync();
+        [HttpPost("")]
+        public async Task<IActionResult> AddAnnouncement([FromBody] CreateAnnouncementRequest announcement)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
 
-//            return CreatedAtAction(nameof(GetAnnouncementGroupAnnouncements), new { announcementGroupId = result.AnnouncementGroupId }, result);
+            var result = _mapper.Map<Announcement>(announcement);
 
-//        }
+            await _unitOfWork.Announcements.Add(result);
+            await _unitOfWork.CompleteAsync();
 
-//        [HttpPut("")]
-//        public async Task<IActionResult> UpdateAnnouncement([FromBody] UpdateAnnouncementRequest announcement)
-//        {
-//            if (!ModelState.IsValid)
-//                return BadRequest();
+            return CreatedAtAction(nameof(GetAnnouncementGroupAnnouncements), new { announcementGroupId = result.AnnouncementGroupId }, result);
 
-//            var result = _mapper.Map<Announcement>(announcement);
+        }
 
-//            await _unitOfWork.Announcements.Update(result);
-//            await _unitOfWork.CompleteAsync();
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateAnnouncement([FromBody] UpdateAnnouncementRequest announcement)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
 
-//            return NoContent();
-//        }
-//    }
-//}
+            var result = _mapper.Map<Announcement>(announcement);
+
+            await _unitOfWork.Announcements.Update(result);
+            await _unitOfWork.CompleteAsync();
+
+            return NoContent();
+        }
+    }
+}
