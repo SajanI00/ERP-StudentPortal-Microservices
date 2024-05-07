af155ee3247fff87b1b8dfbf25ecc6fd66b30c44
Sandali <sandali811@gmail.com>
2024-05-07 13:25:21 +0530

    created announcement repositories

--- ERP-BaseApp/src/Announcements/ERP.Announcements.APi/Controllers/BaseController.cs
+++ ERP-BaseApp/src/Announcements/ERP.Announcements.APi/Controllers/BaseController.cs
@@ -1,22 +1,22 @@
-﻿//using AutoMapper;
-//using ERP.Announcements.Core.Interfaces;
-//using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using ERP.Announcements.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace ERP.Announcements.Api.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class BaseController : ControllerBase
-//    {
-//        protected readonly IUnitOfWork _unitOfWork;
-//        protected readonly IMapper _mapper;
+namespace ERP.Announcements.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+        protected readonly IMapper _mapper;
 
-//        public BaseController(
-//            IUnitOfWork unitOfWork,
-//            IMapper mapper)
-//        {
-//            _unitOfWork = unitOfWork;
-//            _mapper = mapper;
-//        }
-//    }
-//}
+        public BaseController(
+            IUnitOfWork unitOfWork,
+            IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+    }
+}
--- ERP-BaseApp/src/Announcements/ERP.Announcements.APi/Controllers/StudentController.cs
+++ ERP-BaseApp/src/Announcements/ERP.Announcements.APi/Controllers/StudentController.cs
@@ -1,87 +1,87 @@
-﻿//using AutoMapper;
-//using ERP.Announcements.Core.DTOs.Request;
-//using ERP.Announcements.Core.DTOs.Response;
-//using ERP.Announcements.Core.Entity;
-//using ERP.Announcements.Core.Interfaces;
-//using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using ERP.Announcements.Core.DTOs.Request;
+using ERP.Announcements.Core.DTOs.Response;
+using ERP.Announcements.Core.Entity;
+using ERP.Announcements.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace ERP.Announcements.Api.Controllers
-//{
-//    public class StudentController : BaseController
-//    {
-//        public StudentController(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
-//        {
-//        }
+namespace ERP.Announcements.Api.Controllers
+{
+    public class StudentController : BaseController
+    {
+        public StudentController(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+        {
+        }
 
-//        [HttpGet]
-//        [Route("{studentId:guid}")]
-//        public async Task<IActionResult> GetStudentMethod(Guid studentId)
-//        {
-//            var student = await _unitOfWork.Students.GetById(studentId);
+        [HttpGet]
+        [Route("{studentId:guid}")]
+        public async Task<IActionResult> GetStudentMethod(Guid studentId)
+        {
+            var student = await _unitOfWork.Students.GetById(studentId);
 
-//            if (student == null)
-//            {
-//                return NotFound();
-//            }
+            if (student == null)
+            {
+                return NotFound();
+            }
 
-//            var result = _mapper.Map<GetStudentResponse>(student);
+            var result = _mapper.Map<GetStudentResponse>(student);
 
-//            return Ok(result);
-//        }
+            return Ok(result);
+        }
 
-//        [HttpPost("")]
-//        public async Task<IActionResult> AddStudentMethod([FromBody] CreateStudentRequest student)
-//        {
-//            if (!ModelState.IsValid)
-//                return BadRequest();
+        [HttpPost("")]
+        public async Task<IActionResult> AddStudentMethod([FromBody] CreateStudentRequest student)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
 
-//            var result = _mapper.Map<Student>(student);
+            var result = _mapper.Map<Student>(student);
 
-//            await _unitOfWork.Students.Add(result);
-//            await _unitOfWork.CompleteAsync();
+            await _unitOfWork.Students.Add(result);
+            await _unitOfWork.CompleteAsync();
 
-//            return CreatedAtAction(nameof(GetStudentMethod), new { studentId = result.Id }, result);
+            return CreatedAtAction(nameof(GetStudentMethod), new { studentId = result.Id }, result);
 
-//        }
+        }
 
-//        [HttpPut("")]
-//        public async Task<IActionResult> UpdateStudentMethod([FromBody] UpdateStudentRequest student)
-//        {
-//            if (!ModelState.IsValid)
-//                return BadRequest();
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateStudentMethod([FromBody] UpdateStudentRequest student)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
 
-//            var result = _mapper.Map<Student>(student);
+            var result = _mapper.Map<Student>(student);
 
-//            await _unitOfWork.Students.Update(result);
-//            await _unitOfWork.CompleteAsync();
+            await _unitOfWork.Students.Update(result);
+            await _unitOfWork.CompleteAsync();
 
-//            return NoContent();
+            return NoContent();
 
-//        }
+        }
 
-//        [HttpGet]
-//        public async Task<IActionResult> GetAllStudentsMethod()
-//        {
-//            var student = await _unitOfWork.Students.GetAll();
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudentsMethod()
+        {
+            var student = await _unitOfWork.Students.GetAll();
 
-//            return Ok(_mapper.Map<IEnumerable<GetStudentResponse>>(student));
-//        }
+            return Ok(_mapper.Map<IEnumerable<GetStudentResponse>>(student));
+        }
 
-//        [HttpDelete]
-//        [Route("{studentId:guid}")]
-//        public async Task<IActionResult> DeleteStudentMethod(Guid studentId)
-//        {
-//            var student = await _unitOfWork.Students.GetById(studentId);
+        [HttpDelete]
+        [Route("{studentId:guid}")]
+        public async Task<IActionResult> DeleteStudentMethod(Guid studentId)
+        {
+            var student = await _unitOfWork.Students.GetById(studentId);
 
-//            if (student == null)
-//            {
-//                return NotFound();
-//            }
+            if (student == null)
+            {
+                return NotFound();
+            }
 
-//            await _unitOfWork.Students.Delete(studentId);
-//            await _unitOfWork.CompleteAsync();
+            await _unitOfWork.Students.Delete(studentId);
+            await _unitOfWork.CompleteAsync();
 
-//            return NoContent();
-//        }
-//    }
-//}
+            return NoContent();
+        }
+    }
+}
